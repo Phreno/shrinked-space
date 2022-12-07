853103b29326d81fb43a4b65be5410fef8d56d91
phreno <losgoomy@gmail.com>
2022-12-07 21:35:20 +0100

    ajoute la vue en perspective

--- src/grid/grid.rs
+++ src/grid/grid.rs
@@ -1,6 +1,6 @@
 // import cell
 use crate::grid::cell::Cell;
-use nalgebra::{Const, Matrix3, OPoint, Point3, Vector3};
+use nalgebra::{Const, Matrix3, OPoint, Point2, Point3, Vector3};
 use perlin2d::PerlinNoise2D;
 pub struct Grid {
     pub width: u32,
@@ -26,24 +26,41 @@ impl Grid {
         self.cells[self.get_cell_index(y, x)]
     }
 
+    pub(crate) fn to_3_points_perspective(&self, get: Cell) -> Point2<f64> {
+        let point = self.get_3d_point_at(get);
+        self.get_3_points_perspective_at(point)
+    }
+
     fn get_cell_index(&self, y: i32, x: i32) -> usize {
         (y * self.width as i32 + x) as usize
     }
 
-    pub(crate) fn perlin_noise_at(&self, cell: Cell) -> f64 {
+    fn perlin_noise_at(&self, cell: Cell) -> f64 {
         let noise = self
             .perlin
             .get_noise(cell.get_x_as_f64(), cell.get_y_as_f64());
         noise
     }
 
-    pub(crate) fn get_3d_point_at(&self, get: Cell) -> OPoint<f64, Const<3>> {
+    fn get_3d_point_at(&self, get: Cell) -> OPoint<f64, Const<3>> {
         let point = Point3::new(
             get.get_x_as_f64(),
             get.get_y_as_f64(),
             self.perlin_noise_at(get),
         );
         point
+    }
+
+    fn get_3_points_perspective_at(&self, point: OPoint<f64, Const<3>>) -> Point2<f64> {
+        let vx = point.x / 2.0;
+        let vy = point.y / (point.z / 10.0);
+
+        // Calculate the coordinates of the projected cube
+        let px = point.x / (point.z / 10.0);
+        let py = point.y / (point.z / 10.0);
+
+        // Return the projected coordinates
+        Point2::new(px, py)
     }
 }
 
@@ -59,6 +76,8 @@ fn render_grid(width: i32, height: i32) -> Vec<Cell> {
 
 #[cfg(test)]
 mod tests {
+    use nalgebra::Point2;
+
     use super::*;
 
     #[test]
@@ -86,9 +105,24 @@ mod tests {
     }
 
     #[test]
-    fn get_3d_point_at_should_return_a_point() {
+    fn get_3d_point_at_should_return_a_3Dpoint() {
         let grid = Grid::new(10, 10);
         let point = grid.get_3d_point_at(grid.get(0, 1));
         assert_eq!(point, Point3::new(0.0, 1.0, 2.890339412503229));
+    }
+
+    #[test]
+    fn get_3_points_perspective_at_should_return_a_2Dpoint() {
+        let grid = Grid::new(10, 10);
+        let point = grid.get_3d_point_at(grid.get(0, 1));
+        let projection = grid.get_3_points_perspective_at(point);
+        assert_eq!(projection, Point2::new(0.0, 3.4598012803414413));
+    }
+
+    #[test]
+    fn to_3_points_perspective_should_return_a_2Dpoint() {
+        let grid = Grid::new(10, 10);
+        let projection = grid.to_3_points_perspective(grid.get(0, 1));
+        assert_eq!(projection, Point2::new(0.0, 3.4598012803414413));
     }
 }
